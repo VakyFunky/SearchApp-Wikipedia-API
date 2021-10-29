ce9d7189944853dc34bbcf5f4d3567b299627090
vakyfunky <cve.vanja@gmail.com>
2021-10-29 23:15:31 +0200

    master

---
+++ js/searchBar.js
@@ -0,0 +1,20 @@
+export const setSearchFocus = () => {
+    document.getElementById('search').focus();
+}
+
+export const showClearTextButton = () => {
+    const search = document.getElementById("search");
+    const clear = document.getElementById('clear');
+    if (search.value.length){
+        clear.classList.remove("none");
+        clear.classList.add("flex");
+    } else {
+        clear.classList.add("none");
+        clear.classList.remove("flex");
+    }
+}
+
+export const clearSearchText = (e) => {
+    e.preventDefault();
+    document.getElementById("search").value = "";
+}
